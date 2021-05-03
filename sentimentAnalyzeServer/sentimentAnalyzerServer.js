2894843c54202bd3178013bb4da6ee7afa279953
PTamm <tamm@ualberta.ca>
2021-05-03 07:25:35 +0000

    further changes

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -92,14 +92,60 @@ const analyzeParams = {
 
 app.get("/text/emotion", (req,res) => {
     // return res.send({"happy":"10","sad":"90"});
+const analyzeParams = {
+  'url': req.query.text,
+  'features': {
+    'entities': {
+      'emotion': true,
+      'sentiment': true,
+      'limit': 2,
+    },
+    'keywords': {
+      'emotion': true,
+      'sentiment': true,
+      'limit': 2,
+    },
+  },
+};
     nlu_txt_em = getNLUInstance();
-    return res.send(nlu_txt_em.analyze(req.query.text));
+    nlu_txt_em.analyze(analyzeParams)
+    .then(analysisResults => {
+        return res.send(JSON.stringify(analysisResults, null, 2));
+    })
+    .catch(err => {
+        return res.send({"happy":"90","sad":"10"});
+        // console.log('error:', err);
+    });
+    // return res.send(nlu_txt_em.analyze(req.query.text));
 });
 
 app.get("/text/sentiment", (req,res) => {
     // return res.send("text sentiment for "+req.query.text);
+const analyzeParams = {
+  'url': req.query.text,
+  'features': {
+    'entities': {
+      'emotion': true,
+      'sentiment': true,
+      'limit': 2,
+    },
+    'keywords': {
+      'emotion': true,
+      'sentiment': true,
+      'limit': 2,
+    },
+  },
+};
     nlu_txt_se = getNLUInstance();
-    return res.send(nlu_txt_se.analyze(req.query.text));
+    nlu_txt_se.analyze(analyzeParams)
+    .then(analysisResults => {
+        return res.send(JSON.stringify(analysisResults, null, 2));
+    })
+    .catch(err => {
+        return res.send({"happy":"90","sad":"10"});
+        // console.log('error:', err);
+    });
+    // return res.send(nlu_txt_se.analyze(req.query.text));
 });
 
 let server = app.listen(8080, () => {
